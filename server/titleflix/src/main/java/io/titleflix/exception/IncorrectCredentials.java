972bd87f5fb61385736653a03e064bf478eebc9f
kaushik nandhan injamuri <kaushik nandhan injamuri>
2016-08-15 21:02:01 -0700

    feat(module-api): add unauthorization for incorrect credentials

--- server/titleflix/src/main/java/io/titleflix/exception/IncorrectCredentials.java
+++ server/titleflix/src/main/java/io/titleflix/exception/IncorrectCredentials.java
@@ -3,7 +3,7 @@ package io.titleflix.exception;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.http.HttpStatus;
 
-@ResponseStatus(code=HttpStatus.BAD_REQUEST,reason="Incorrect Credentials for the Email and Password")
+@ResponseStatus(code=HttpStatus.UNAUTHORIZED,reason="Incorrect Credentials for the Email and Password")
 public class IncorrectCredentials extends Exception {
 
 	/**
